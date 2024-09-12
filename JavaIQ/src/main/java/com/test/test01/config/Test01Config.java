33c6b02deaa1cd4426562719665f8f84f1ef644e
Leela-Prasad <leela.jagu@gmail.com>
2024-09-12 12:15:33 +0530

    Ignoring Null fields when sending response to client

--- JavaIQ/src/main/java/com/test/test01/config/Test01Config.java
+++ JavaIQ/src/main/java/com/test/test01/config/Test01Config.java
@@ -1,6 +1,7 @@
 package com.test.test01.config;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,7 @@ public class Test01Config {
     public ObjectMapper objectMapper() {
         var objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return objectMapper;
     }
 }

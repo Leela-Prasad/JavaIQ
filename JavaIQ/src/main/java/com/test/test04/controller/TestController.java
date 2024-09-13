b24aff2c10915849d7b491d1b83f1a6c3f764f92
Leela-Prasad <leela.jagu@gmail.com>
2024-09-13 16:13:50 +0530

    Role Based Access

--- JavaIQ/src/main/java/com/test/test04/controller/TestController.java
+++ JavaIQ/src/main/java/com/test/test04/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.test.test04.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @PreAuthorize("hasAuthority('ROLE_EDITOR')")
     @GetMapping("/test")
     public String test() {
         return "Test Controller Executed";

7fe162e638af88f8e866f2266376519c92524cd9
Leela-Prasad <leela.jagu@gmail.com>
2024-09-13 16:17:29 +0530

    Session Invalidation in Logout Controller

--- JavaIQ/src/main/java/com/test/test04/controller/LogoutController.java
+++ JavaIQ/src/main/java/com/test/test04/controller/LogoutController.java
@@ -4,11 +4,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class LogoutController {
 
     @GetMapping("/custom-logout")
-    public String logout() {
+    public String logout(HttpSession session) {
+        session.invalidate();
         SecurityContextHolder.clearContext();
         return "Session Cleared";
     }
